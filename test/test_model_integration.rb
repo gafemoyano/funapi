31771da17fbe4ce266cb5c21efe5601395458631
Felipe <gafemoyano@gmail.com>
2026-07-18 13:56:08 -0500

    Phase 5: migrate test_fun_api and test_model_integration to TestClient

--- test/test_model_integration.rb
+++ test/test_model_integration.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "funapi/test_client"
 
 class IntAddress < FunApi::Model
   field :street, :string
@@ -19,78 +20,63 @@ class IntUserOut < FunApi::Model
 end
 
 class TestModelIntegration < Minitest::Test
-  def async_request(app, method, path, **options)
-    Async do
-      Rack::MockRequest.new(app).send(method, path, **options)
-    end.wait
-  end
-
-  def parse(response)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
   def build_app
     FunApi::App.new do |api|
-      api.post "/users", body: IntUserIn, response_schema: IntUserOut do |input, _req, _task|
+      api.post "/users", body: IntUserIn, response_schema: IntUserOut do |input, _req|
         record = Struct.new(:id, :name, :email, :password)
           .new(123, input[:body][:name], input[:body][:email], "secret")
         [record, 201]
       end
 
-      api.get "/users", response_schema: [IntUserOut] do |_input, _req, _task|
+      api.get "/users", response_schema: [IntUserOut] do |_input, _req|
         [[{id: 1, name: "Al", email: "a@b.com", password: "x"}], 200]
       end
     end
   end
 
+  def client(app = build_app)
+    FunApi::TestClient.new(app)
+  end
+
   def test_request_validation_with_model
-    app = build_app
-    res = async_request(app, :post, "/users",
-      "CONTENT_TYPE" => "application/json",
-      :input => {name: "Al", email: "a@b.com"}.to_json)
+    res = client.post("/users", json: {name: "Al", email: "a@b.com"})
 
     assert_equal 201, res.status
-    data = parse(res)
+    data = res.json
     assert_equal 123, data[:id]
     assert_equal "Al", data[:name]
     refute data.key?(:password)
   end
 
   def test_request_validation_failure_returns_422
-    app = build_app
-    res = async_request(app, :post, "/users",
-      "CONTENT_TYPE" => "application/json",
-      :input => {email: "a@b.com"}.to_json)
+    res = client.post("/users", json: {email: "a@b.com"})
 
     assert_equal 422, res.status
-    data = parse(res)
-    assert(data[:detail].any? { |e| e[:loc].include?("name") })
+    assert(res.json[:detail].any? { |e| e[:loc].include?("name") })
   end
 
   def test_response_array_of_models_filters
-    app = build_app
-    res = async_request(app, :get, "/users")
+    res = client.get("/users")
 
     assert_equal 200, res.status
-    data = parse(res)
+    data = res.json
     assert_equal 1, data.length
     refute data.first.key?(:password)
   end
 
   def test_response_missing_required_field_is_500
     app = FunApi::App.new do |api|
-      api.get "/broken", response_schema: IntUserOut do |_input, _req, _task|
+      api.get "/broken", response_schema: IntUserOut do |_input, _req|
         [{name: "Al", email: "a@b.com"}, 200]
       end
     end
 
-    res = async_request(app, :get, "/broken")
+    res = client(app).get("/broken")
     assert_equal 500, res.status
   end
 
   def test_openapi_uses_model_class_name
-    app = build_app
-    spec = parse(async_request(app, :get, "/openapi.json"))
+    spec = client.get("/openapi.json").json
 
     assert spec[:components][:schemas].key?(:IntUserIn)
     assert spec[:components][:schemas].key?(:IntUserOut)
@@ -100,8 +86,7 @@ class TestModelIntegration < Minitest::Test
   end
 
   def test_openapi_nested_model_inlined
-    app = build_app
-    spec = parse(async_request(app, :get, "/openapi.json"))
+    spec = client.get("/openapi.json").json
     user_in = spec[:components][:schemas][:IntUserIn]
     assert_equal "object", user_in[:properties][:address][:type]
   end
@@ -112,17 +97,15 @@ class TestModelIntegration < Minitest::Test
     end
 
     app = FunApi::App.new do |api|
-      api.post "/legacy", body: legacy do |input, _req, _task|
+      api.post "/legacy", body: legacy do |input, _req|
         [{received: input[:body][:name]}, 200]
       end
     end
 
-    res = async_request(app, :post, "/legacy",
-      "CONTENT_TYPE" => "application/json",
-      :input => {name: "Legacy"}.to_json)
+    res = client(app).post("/legacy", json: {name: "Legacy"})
 
     assert_equal 200, res.status
-    assert_equal "Legacy", parse(res)[:received]
+    assert_equal "Legacy", res.json[:received]
   end
 
   def test_model_query_params
@@ -132,14 +115,14 @@ class TestModelIntegration < Minitest::Test
     end
 
     app = FunApi::App.new do |api|
-      api.get "/search", query: query_model do |input, _req, _task|
+      api.get "/search", query: query_model do |input, _req|
         [{q: input[:query][:q], limit: input[:query][:limit]}, 200]
       end
     end
 
-    res = async_request(app, :get, "/search?q=ruby&limit=5")
+    res = client(app).get("/search", params: {q: "ruby", limit: 5})
     assert_equal 200, res.status
-    data = parse(res)
+    data = res.json
     assert_equal "ruby", data[:q]
     assert_equal 5, data[:limit]
   end
